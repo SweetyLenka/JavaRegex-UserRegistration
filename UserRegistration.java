8264497729b09c972e03c299903f9d999cd45ad5
sweetylenka <sweetylenka2013@gmail.com>
2021-03-29 19:28:39 +0530

    As a User need to enter a valid First Name - First name starts with Cap and has minimum 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,11 +1,30 @@
 package userreg;
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class UserRegistration {
 	//@Description User Registration System needs to ensure all validations are in place during the User Entry
 	
+	public static void validateFirstName()
+	{
+		
+		System.out.println("*******FIRST NAME*******\n * First Name should start with a capital lettler \n * First Name should have minimum 3 characters \n");
+		System.out.println("Enter a valid First Name :");
+		
+		Scanner sc = new Scanner(System.in);
+		String firstName=sc.nextLine();
+		String regexfirstName = "[A-Z]{1}[a-z]{2,}";
+		boolean firstNameValidator = firstName.matches(regexfirstName);
+		if(firstNameValidator)
+			System.out.println(firstName+" is a valid First Name");
+		else
+			System.out.println(firstName+" is not a valid First Name");
+	}
 	
 	public static void main(String args[]) {
 		System.out.println("WELCOME TO USER REGISTRATION VALIDATION");
-}
-
+		UserRegistration userreg = new UserRegistration();
+		userreg.validateFirstName();
+		
+	}
 }
