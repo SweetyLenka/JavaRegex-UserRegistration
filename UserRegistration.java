f77e6e730e2d81f627abcdec2d38c53eb84d9d0c
sweetylenka <sweetylenka2013@gmail.com>
2021-03-29 16:49:22 +0530

    USER REGISTRATION VALIDATION'

---
+++ UserRegistration.java
@@ -0,0 +1,11 @@
+package userreg;
+
+public class UserRegistration {
+	//@Description User Registration System needs to ensure all validations are in place during the User Entry
+	
+	
+	public static void main(String args[]) {
+		System.out.println("WELCOME TO USER REGISTRATION VALIDATION");
+}
+
+}
