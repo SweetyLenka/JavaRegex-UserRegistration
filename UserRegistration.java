27fcf0e537d0c620c9c3a385bc28ee1ea918d671
sweetylenka <sweetylenka2013@gmail.com>
2021-03-29 19:42:30 +0530

    UC3 : As a User need to enter
    a valid Last
    Name - Last name starts with Cap and has
    minimum 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -16,15 +16,45 @@ public class UserRegistration {
 		String regexfirstName = "[A-Z]{1}[a-z]{2,}";
 		boolean firstNameValidator = firstName.matches(regexfirstName);
 		if(firstNameValidator)
+		{
 			System.out.println(firstName+" is a valid First Name");
+		}
 		else
+		{
 			System.out.println(firstName+" is not a valid First Name");
+			validateFirstName();
+		}
+			
+		
 	}
+	public static void validateLastName()
+	{
+		System.out.println("*******LAST NAME*******\n * Last Name should start with a capital lettler \n * Last Name should have minimum 3 characters \n");
+		System.out.println("Enter a valid Last Name :");
+		
+		Scanner sc = new Scanner(System.in);
+		String lastName=sc.nextLine();
+		String regexlastName = "[A-Z]{1}[a-z]{2,}";
+		boolean firstNameValidator = lastName.matches(regexlastName);
+		if(firstNameValidator)
+		{
+			System.out.println(lastName+" is a valid Last Name");
+		}
+		else
+		{
+			System.out.println(lastName+" is not a valid Last Name");
+			validateLastName();
+		}
+	}
+		
+		
+	
 	
 	public static void main(String args[]) {
 		System.out.println("WELCOME TO USER REGISTRATION VALIDATION");
 		UserRegistration userreg = new UserRegistration();
 		userreg.validateFirstName();
+		userreg.validateLastName();
 		
 	}
 }
