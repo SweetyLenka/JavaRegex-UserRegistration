c740a9b2c2c0ed6723bb0690ee80e8b29a62c535
sweetylenka <sweetylenka2013@gmail.com>
2021-04-05 12:35:26 +0530

    UC2 - As a User need to enter
    a valid Last
    Name - Last name starts with Cap and has
    minimum 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.*;
 
 public class UserRegistration {
 	private static String firstName;
+	private static String lastName;
 	Scanner sc=new Scanner(System.in);
 
 	//Method to get First Name
@@ -16,25 +17,49 @@ public class UserRegistration {
 		String fname=sc.nextLine();
 		return fname;
 	}
+	//Method to get Last Name
+		public String getLastName()
+		{
+			System.out.println("*******LAST NAME*******\n * Last Name should start with a capital lettler \n * Last Name should have minimum 3 characters \n");
+			System.out.println("Enter a valid Last Name :");
+			String lname=sc.nextLine();
+			return lname;
+		}
+	
 	//Method to check whether First name is valid
-	public void validateFirstName(String name) {
-	String regexfirstName = "[A-Z]{1}[a-z]{2,}";
-	boolean firstNameValidator = name.matches(regexfirstName);
-	if(firstNameValidator)
-	{
-		System.out.println(name+" is a valid First Name");
-	}
-	else
-	{
-		System.out.println(name+" is not a valid First Name");
-	}
-	}
+		public void validateFirstName(String name) {
+			String regexfirstName = "[A-Z]{1}[a-z]{2,}";
+			boolean firstNameValidator = name.matches(regexfirstName);
+			if(firstNameValidator)
+			{
+				System.out.println(name+" is a valid First Name");
+			}
+			else
+			{
+				System.out.println(name+" is not a valid First Name");
+			}
+			}
+		//Method to check whether Last name is valid
+				public void validateLastName(String name) {
+					String regexlastName = "[A-Z]{1}[a-z]{2,}";
+					boolean lastNameValidator = name.matches(regexlastName);
+					if(lastNameValidator)
+					{
+						System.out.println(name+" is a valid Last Name");
+					}
+					else
+					{
+						System.out.println(name+" is not a valid Last Name");
+					}
+					}
+			
 	
 	public static void main(String[] args) {
 		UserRegistration usereg=new UserRegistration();
 		firstName=usereg.getFirstName();
 		usereg.validateFirstName(firstName);
-		
+		lastName=usereg.getLastName();
+		usereg.validateLastName(lastName);
     }
 	
 }
