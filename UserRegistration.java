1caa84c9bbd0f11e5f2de52b371769a15abc7878
sweetylenka <sweetylenka2013@gmail.com>
2021-04-05 12:07:29 +0530

    Updated - First Name Validator

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,11 +5,10 @@ import java.util.regex.*;
 //@Description User Registration System needs to ensure all validations are in place during the User Entry
 
 public class UserRegistration {
+	private static String firstName;
 	Scanner sc=new Scanner(System.in);
 
-	
-	 
-	
+	//Method to get First Name
 	public String getFirstName()
 	{
 		System.out.println("*******FIRST NAME*******\n * First Name should start with a capital lettler \n * First Name should have minimum 3 characters \n");
@@ -17,7 +16,7 @@ public class UserRegistration {
 		String fname=sc.nextLine();
 		return fname;
 	}
-	
+	//Method to check whether First name is valid
 	public void validateFirstName(String name) {
 	String regexfirstName = "[A-Z]{1}[a-z]{2,}";
 	boolean firstNameValidator = name.matches(regexfirstName);
@@ -28,13 +27,12 @@ public class UserRegistration {
 	else
 	{
 		System.out.println(name+" is not a valid First Name");
-		//validateFirstName(String name);
 	}
 	}
 	
 	public static void main(String[] args) {
 		UserRegistration usereg=new UserRegistration();
-		String firstName=usereg.getFirstName();
+		firstName=usereg.getFirstName();
 		usereg.validateFirstName(firstName);
 		
     }
