753d833257f0d64ec2a8d34b760053f6206d92e9
sweetylenka <sweetylenka2013@gmail.com>
2021-04-05 15:50:24 +0530

    Added : UC4 As a User need to follow pre
    -defined
    Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10
    digit number

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,31 +9,41 @@ public class UserRegistration {
 	private static String lastName;
 	private static String email;
 	private static String password;
+	private static String mobile_no;
 	Scanner sc=new Scanner(System.in);
 
 	//Method to get First Name
 	public String getFirstName()
-	{
+		{
 		System.out.println("*******FIRST NAME*******\n * First Name should start with a capital lettler \n * First Name should have minimum 3 characters \n");
 		System.out.println("Enter a valid First Name :");
 		String fname=sc.nextLine();
 		return fname;
-	}
+		}
 	//Method to get Last Name
 		public String getLastName()
-		{
+			{
 			System.out.println("*******LAST NAME*******\n * Last Name should start with a capital lettler \n * Last Name should have minimum 3 characters \n");
 			System.out.println("Enter a valid Last Name :");
 			String lname=sc.nextLine();
 			return lname;
-		}
+			}
 		//Method to get Email
-		public String getEmail() {
+		public String getEmail() 
+			{
 			System.out.println("*******EMAIL ID *******\n Email has 3 mandatory parts (abc, bl\r\n precise @ and . positions )\n");
 			System.out.println("Enter a valid Email:");
 			String email=sc.nextLine();
 			return email;
-		}
+			}
+		//Method to get mobile_number
+		public String getMobileNo()
+			{
+			System.out.println("*******MOBILE NUMBER*******\n * Mobile Format - Country code follow by space and 10 digit number\n");
+			System.out.println("Enter a valid MOBILE NUMBER :");
+			String mobile_no=sc.nextLine();
+			return mobile_no;
+			}
 	
 	//Method to check whether First name is valid
 		public void validateFirstName(String name) {
@@ -73,10 +83,23 @@ public class UserRegistration {
 			{
 				System.out.println(emailID+" is not a valid Email Id");
 			}
+			}
+			//Method to check whether mobile_no is valid
+			public void mobilevalidation(String mobile) {
+			String regexmobile = "[9]{1}[1]{1}[\s]{1}[0-9]{10}";
+			boolean mobileValidator = mobile.matches(regexmobile);
+			if(mobileValidator)
+			{
+				System.out.println(mobile+" is a valid Mobile Number");
+			}
+			else
+			{
+				System.out.println(mobile+" is not a valid Mobile Number");
+		
+			}
+			}
 			
-		}
-		
-		
+			
 	public static void main(String[] args) {
 		UserRegistration usereg=new UserRegistration();
 		firstName=usereg.getFirstName();
@@ -85,6 +108,8 @@ public class UserRegistration {
 		usereg.validateLastName(lastName);
 		email=usereg.getEmail();
 		usereg.validateEmail(email);
+		mobile_no=usereg.getMobileNo();
+		usereg.mobilevalidation(mobile_no);
     }
 	
 }
